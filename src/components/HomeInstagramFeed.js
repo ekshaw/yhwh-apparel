795dcf6ce34cb0e30d0c1304bd3ef61357e80035
mscott2757 <mscott2757@berkeley.edu>
2018-01-17 13:03:04 -0800

    added instagram feed

---
+++ src/components/HomeInstagramFeed.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Instafeed from 'react-instafeed';
+
+class HomeInstagramFeed extends Component {
+  render() {
+    const instafeedTarget = 'instafeed';
+    const instafeedSettings = {
+      limit: '9',
+      ref: 'instafeed',
+      resolution: 'standard_resolution',
+      sortBy: 'most-recent',
+      target: instafeedTarget,
+      template: '<div class="home-instafeed__img"><a href="{{link}}"><img src="{{image}}" width=100%></a></div>',
+      userId: '2189584280',
+      accessToken: '2189584280.1677ed0.cf0a6d5a22d24fa7b54114142f5f10a3'
+    }
+
+    return (
+      <div className='home-instafeed-container'>
+        <div className='home-instafeed__title'>
+          <div />
+          <span>Instagram</span>
+          <div />
+        </div>
+        <div className='home-instafeed__scroll-container'>
+          <div id={instafeedTarget} className='home-instafeed'>
+            <Instafeed {...instafeedSettings}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeInstagramFeed;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomeCarousel from '../components/HomeCarousel';
 import HomeProducts from '../components/HomeProducts';
+import HomeInstagramFeed from '../components/HomeInstagramFeed';
 
 
 class Home extends Component {
@@ -9,6 +10,7 @@ class Home extends Component {
       <div className='home'>
         <HomeCarousel />
         <HomeProducts />
+        <HomeInstagramFeed />
       </div>
     );
   }
