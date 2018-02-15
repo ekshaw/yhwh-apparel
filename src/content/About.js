8f65db6870d7bdd6f55e7f13048c31e8e58330d2
Dominic Fannjiang <dfannjiang@gmail.com>
2018-02-15 08:04:16 -0800

    edit about description slightly

--- src/content/About.js
+++ src/content/About.js
@@ -3,7 +3,7 @@ const about = {
     'YHWH was created out of a love made known, a need that was seen, and a call that was heard (Psalm 107) YHWH is the tetragrammaton of one of the names of God, often spoken as "Yahweh" (Exodus 3:14). It was (and still is, by certain groups) regarded too holy to be spoken.',
     'Our desire is simply to share the goodness and truth of the Gospel. To tell the world of the beautiful name of God that they may seek and find that He is Lord and that they may discover the unconditional love that is for them.',
     'Our vision is to speak truth through our apparel, empowering people to express their identity as children of the Most High King, and to encourage believers to be bold as they carry the name of their Lord, not only on their shirts, but on their hearts. It is also a call to live a life of consistency before our God.',
-    'If you would like to start your own YHWH Apparel chapter at your college, please send us an email.'
+    'If you have any questions, or if you would like to start your own YHWH Apparel chapter at your college, please send us an email!'
   ],
   videoId: 'rej9vc7i__4',
   email: 'yhwhapparel@gmail.com',
