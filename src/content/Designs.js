541d70448fad359addb55da9d945a9eb2e36a776
Dominic Fannjiang <dfannjiang@gmail.com>
2018-02-14 18:17:50 -0800

    Updated designs descriptions

--- src/content/Designs.js
+++ src/content/Designs.js
@@ -7,7 +7,10 @@ const designs = [
     description:
       <div>
         <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+         SELAH means to “pause and praise”, carefully weighing the value of what
+				 has just been said. Our team created SELAH to remind ourselves to have a
+				 posture of measuring the weight of the Word, pausing, and turning it back
+				 into praise.
         </p>
       </div>,
     imgSrc: require('../images/designs/selah.png')
@@ -17,8 +20,22 @@ const designs = [
     release: 'April 2017',
     description:
       <div>
-        <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+        <p> 
+          To seek. 
+        </p>
+		    <p> 
+          Is to search after. 
+        </p>
+			  <p> 
+          When the waves seem to be rising faster. When our heart seems tossed around.
+					When we feel like we keep hitting the same wall over and over again. When there’s no
+					improvement. In him. In her. In us. It leaves us tired and weary, beaten down. 
+        </p>
+		   	<p> 
+          But in times like this, we MUST remind ourselves of His promises. Even if our
+					mind can recite it, our heart may not truly believe it. But by praying and declaring
+					His promises for us, we are speaking TRUTH over ourselves, so that darkness and hopelessness
+					will not overcome us. 
         </p>
       </div>,
     imgSrc: require('../images/designs/seek.png')
@@ -28,8 +45,28 @@ const designs = [
     release: 'March 2016',
     description:
       <div>
-        <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+        <p> 
+          We say we’re athletes, artists, students. We identify ourselves by
+          personality type, and pride ourselves in being perceived as polite,
+          good looking, or well-liked. We tell the world we go to this college, or
+          we’re dating that person; We say, “Look at where I've been. Look at what
+          I've done, and who I’ll surely become. This is who I am.” 
+        </p>
+				<p> 
+          But at the core of it all, who are we really? 
+        </p>
+				<p> 
+          When we grow up, and find ourselves without the comfort of our parents.
+          When our brains can no longer function at the same level of creativity,
+          when the things that once excited us begin to dull. When suddenly our
+          personality or friends or circumstances are no longer the same as they once were.
+        </p>
+				<p> 
+          When there is nothing left of ourselves that we can say or boast of. 
+        </p>
+				<p> 
+          Know what you are. Know who you are. Know Whose you are.
+					You are defined by the Maker. 
         </p>
       </div>,
     imgSrc: require('../images/designs/identity.png')
@@ -39,8 +76,24 @@ const designs = [
     release: 'March 2016',
     description:
       <div>
-        <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+        <p> 
+          Beautiful, majestic King. He rules with grace, even when we cannot
+          fathom the depths of His grace. 
+        </p>
+				<p> 
+          He is powerful and strong. When He roars, the Earth trembles at His might.
+        </p>
+				<p> 
+          He is gentle. His heart longs for intimacy with His people. 
+        </p>
+				<p> 
+          He is the Lion of Judah, whose kingship was demonstrated
+          through His conquering of sin and death at the cross. He is the Lamb
+          who was slain, so that we may spend eternity with Him. He gives boldness
+          to those who believe in Him, that they may have courage to overcome,
+          saying “Fear not, for I am with you.” He has created a kingdom, and has called us
+          to be co-heirs with Christ. He is the King of Glory, worthy to be praised.
+          Holy, righteous, and bold. 
         </p>
       </div>,
     imgSrc: require('../images/designs/lion.png')
@@ -50,8 +103,14 @@ const designs = [
     release: 'March 2016',
     description:
       <div>
-        <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+        <p> 
+          Mountains are often associated with sheer might--the idea that if only
+					we had enough power, we could move huge obstacles. When we face trials
+					that seem to exceed our strengths, let us instead surrender our
+					inabilities at the feet of our Father, whose strength is made perfect
+					in our weakness. From a distance, our daunting challenges seem unshakeable,
+					but when we patiently put our trust in God and approach with His full confidence,
+					we will begin to see how far He has moved our mountains. 
         </p>
       </div>,
     imgSrc: require('../images/designs/mountains.png')
@@ -62,7 +121,22 @@ const designs = [
     description:
       <div>
         <p>
-          SELAH means to “pause and praise”, carefully weighing the value of what has just been said. Our team created SELAH to remind ourselves to have a posture of measuring the weight of the Word, pausing, and turning it back into praise.
+          For our first product, we wanted to establish our hope for YHWH
+					Apparel: to proclaim the name of God. From beginning to the end, this is
+					about our God. He is more than enough.
+        </p>
+				<p>
+          Yahweh, the Hebrew name of God, translates to "I am who I am"
+					(Exodus 3:14). We proclaim His holy name because He first beckoned us
+					and He calls us His children.
+        </p>
+				<p>
+          Don't simply wear this name on your chest. Take up your identity as a
+					child of God. Wear it on your heart. Convey it in your words and actions.
+        </p>
+			 	<p>
+          You are wearing the name of the Most High King. What will you say?
+					What will you do?
         </p>
       </div>,
     imgSrc: require('../images/designs/classic.png')
