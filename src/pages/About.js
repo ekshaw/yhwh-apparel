64dec0e4b8fc48f52d8bc73ffa12b89caaf885ad
Dominic Fannjiang <dfannjiang@gmail.com>
2018-04-01 21:39:23 -0700

    add subscribe button for monthly newsletters on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,6 +16,13 @@ const About = ({ paragraphs, videoId, emailTagline, email }) => {
               {paragraph}
             </p>
           )}
+          <h3>Monthly Newsletters</h3>
+          <p> 
+            As a team, YHWH Apparel wants to engage with our audience more and create relationships with all of you. Through our monthly emails, we hope to keep you updated with our company and the journey we are going through and learn more about you! 
+          </p>
+          <p class="email-subscribe" >
+            <a href="https://yhwhapparel.us17.list-manage.com/subscribe/post?u=3b2e4256bbd2842c224d4af58&amp;id=8889cc916c"> Subscribe </a>
+          </p>
         </div>
         <div className='about-video'>
           <YouTube
