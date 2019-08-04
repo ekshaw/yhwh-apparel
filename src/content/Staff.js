9996c10641481f55a9cab2220d34c118bae1a7e0
Peter Thompson <peter.thompson127@gmail.com>
2019-08-04 00:42:15 -0700

    updated joseph kim's description

--- src/content/Staff.js
+++ src/content/Staff.js
@@ -16,51 +16,46 @@ const staff = {
       description: (
         <div>
           <p>
-            Hi, I'm Hannah and a senior at Cal. I joined YHWH my freshman year
+            "Hi, I'm Hannah and a senior at Cal. I joined YHWH my freshman year
             and have been loving every season of it. I love binging sitcoms,
             funny puppy videos, and learning random facts from the Internet.
-            Hope you are also encouraged by YHWH!
+            Hope you are also encouraged by YHWH!"
           </p>
         </div>
       )
     },
     {
-      name: 'Joseph Kim (Not done)',
-      role: 'Role',
+      name: 'Joseph Kim',
+      role: 'Design',
       imgSrc: require('../images/staff/joseph_kim.jpg'),
       fellowship: 'Passion Livingwater',
       description: (
         <div>
           <p>
-            Before leaving for the trip, we prayed to God that He would show us
-            how He can work through His sons and daughters if they are obedient
-            and put full trust in Him. There were many times throughout this
-            trip where we felt like we were not equipped enough to carry out a
-            certain evangelical task (teaching classrooms, jail ministry,
-            drug/rehab center ministry), but God clearly reminded us that He
-            does not call the equipped, but He equips the called. We had
-            countless valuable conversations with the Filipinos and saw many
-            people accept Jesus into their hearts on this trip. It was an
-            amazing experience living at What The Bible Says Church and spending
-            so much time with the members. It was eye opening for us to see how
-            missional the church members are to their hometown and to see how
-            willing they are to preach the gospel to their neighbors.
+            Joseph Kim has been a designer for YHWH since Spring 2018. He has a
+            strong passion for helping Christians and non believers alike grow
+            deeper in their knowledge of and ultimately love for God. Joseph
+            joined YHWH to use design to communicate themes of God's character
+            and plan of salvation through Jesus. His hope is that through YHWH
+            Apparel's products, Christians would not only be bold in their faith
+            through their outer appearance, but also equipped with Scripture in
+            their hearts to better explain and live out the Gospel.
           </p>
         </div>
       )
     },
     {
       name: 'Lillian Du',
-      role: 'Operations Lead',
+      role: 'Operations',
       imgSrc: require('../images/staff/lillian_du.jpg'),
       fellowship: 'Presence Livingwater',
       description: (
         <div>
           <p>
-            Hello! I'm a senior in Presence and I've been a part of YHWH's
+            "Hello! I'm a senior in Presence and I've been a part of YHWH's
             operations team since Fall '17. I learn something new every semester
             working with YHWH and I'm really excited to see how the team will
-            grow in the coming semesters! 🙂
+            grow in the coming semesters! 🙂"
           </p>
         </div>
       )
@@ -98,11 +93,11 @@ const staff = {
       description: (
         <div>
           <p>
-            Hey everyone! I'm Joseph, and I'm a senior studying MCB and Data
+            "Hey everyone! I'm Joseph, and I'm a senior studying MCB and Data
             Science. I've been a part of YHWH for one year now, and I really
             enjoy that I get to represent what we're passionate about with the
             team. Outside of YHWH and school, I really enjoy fellowshipping with
-            my friends, playing and watching soccer, and eating!
+            my friends, playing and watching soccer, and eating!"
           </p>
         </div>
       )
@@ -115,9 +110,9 @@ const staff = {
       description: (
         <div>
           <p>
-            Hi! My name is Nicole Lee and I am member of YHWH’s marketing team.
+            "Hi! My name is Nicole Lee and I am member of YHWH’s marketing team.
             I am a junior at Cal studying Media Studies, hoping to honor Christ
-            in all that I do.
+            in all that I do."
           </p>
         </div>
       )
@@ -130,12 +125,12 @@ const staff = {
       description: (
         <div>
           <p>
-            Hey, I'm Peter, and I'm currently a junior at Cal studying Computer
+            "Hey, I'm Peter, and I'm currently a junior at Cal studying Computer
             Science. I joined YHWH my sophomore year, and being a part of this
             team and community has been such a blast. It's been so amazing to
             see how we've been able to carry out God's mission through our
             apparel, and I'm so excited to see what lies ahead. Apart from YHWH,
-            I love to play basketball, go snowboarding, and play guitar.
+            I love to play basketball, go snowboarding, and play guitar."
           </p>
         </div>
       )
