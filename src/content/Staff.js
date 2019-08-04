3885347e22452134120b00d6dbe60f737f14e692
Peter Thompson <peter.thompson127@gmail.com>
2019-08-04 16:10:51 -0700

    commented out people who haven't responded yet

--- src/content/Staff.js
+++ src/content/Staff.js
@@ -60,31 +60,31 @@ const staff = {
         </div>
       )
     },
-    {
-      name: 'Millie Ma (Not done)',
-      role: 'Role',
-      imgSrc: require('../images/staff/millie_ma.jpg'),
-      fellowship: 'Fellowship Name',
-      description: (
-        <div>
-          <p>
-            Before leaving for the trip, we prayed to God that He would show us
-            how He can work through His sons and daughters if they are obedient
-            and put full trust in Him. There were many times throughout this
-            trip where we felt like we were not equipped enough to carry out a
-            certain evangelical task (teaching classrooms, jail ministry,
-            drug/rehab center ministry), but God clearly reminded us that He
-            does not call the equipped, but He equips the called. We had
-            countless valuable conversations with the Filipinos and saw many
-            people accept Jesus into their hearts on this trip. It was an
-            amazing experience living at What The Bible Says Church and spending
-            so much time with the members. It was eye opening for us to see how
-            missional the church members are to their hometown and to see how
-            willing they are to preach the gospel to their neighbors.
-          </p>
-        </div>
-      )
-    },
+    // {
+    //   name: 'Millie Ma (Not done)',
+    //   role: 'Role',
+    //   imgSrc: require('../images/staff/millie_ma.jpg'),
+    //   fellowship: 'Fellowship Name',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         Before leaving for the trip, we prayed to God that He would show us
+    //         how He can work through His sons and daughters if they are obedient
+    //         and put full trust in Him. There were many times throughout this
+    //         trip where we felt like we were not equipped enough to carry out a
+    //         certain evangelical task (teaching classrooms, jail ministry,
+    //         drug/rehab center ministry), but God clearly reminded us that He
+    //         does not call the equipped, but He equips the called. We had
+    //         countless valuable conversations with the Filipinos and saw many
+    //         people accept Jesus into their hearts on this trip. It was an
+    //         amazing experience living at What The Bible Says Church and spending
+    //         so much time with the members. It was eye opening for us to see how
+    //         missional the church members are to their hometown and to see how
+    //         willing they are to preach the gospel to their neighbors.
+    //       </p>
+    //     </div>
+    //   )
+    // },
     {
       name: 'Joseph Park',
       role: 'Community Outreach',
@@ -134,57 +134,57 @@ const staff = {
           </p>
         </div>
       )
-    },
-    {
-      name: 'Tai-In Chung (Not done)',
-      role: 'Role',
-      imgSrc: require('../images/staff/tai-in_chung.jpg'),
-      fellowship: 'Fellowship Name',
-      description: (
-        <div>
-          <p>
-            Before leaving for the trip, we prayed to God that He would show us
-            how He can work through His sons and daughters if they are obedient
-            and put full trust in Him. There were many times throughout this
-            trip where we felt like we were not equipped enough to carry out a
-            certain evangelical task (teaching classrooms, jail ministry,
-            drug/rehab center ministry), but God clearly reminded us that He
-            does not call the equipped, but He equips the called. We had
-            countless valuable conversations with the Filipinos and saw many
-            people accept Jesus into their hearts on this trip. It was an
-            amazing experience living at What The Bible Says Church and spending
-            so much time with the members. It was eye opening for us to see how
-            missional the church members are to their hometown and to see how
-            willing they are to preach the gospel to their neighbors.
-          </p>
-        </div>
-      )
-    },
-    {
-      name: 'Sooyoung Park (Not done)',
-      role: 'Role',
-      imgSrc: require('../images/staff/sooyoung_park.jpg'),
-      fellowship: 'Presence Livingwater',
-      description: (
-        <div>
-          <p>
-            Before leaving for the trip, we prayed to God that He would show us
-            how He can work through His sons and daughters if they are obedient
-            and put full trust in Him. There were many times throughout this
-            trip where we felt like we were not equipped enough to carry out a
-            certain evangelical task (teaching classrooms, jail ministry,
-            drug/rehab center ministry), but God clearly reminded us that He
-            does not call the equipped, but He equips the called. We had
-            countless valuable conversations with the Filipinos and saw many
-            people accept Jesus into their hearts on this trip. It was an
-            amazing experience living at What The Bible Says Church and spending
-            so much time with the members. It was eye opening for us to see how
-            missional the church members are to their hometown and to see how
-            willing they are to preach the gospel to their neighbors.
-          </p>
-        </div>
-      )
     }
+    // {
+    //   name: 'Tai-In Chung (Not done)',
+    //   role: 'Role',
+    //   imgSrc: require('../images/staff/tai-in_chung.jpg'),
+    //   fellowship: 'Fellowship Name',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         Before leaving for the trip, we prayed to God that He would show us
+    //         how He can work through His sons and daughters if they are obedient
+    //         and put full trust in Him. There were many times throughout this
+    //         trip where we felt like we were not equipped enough to carry out a
+    //         certain evangelical task (teaching classrooms, jail ministry,
+    //         drug/rehab center ministry), but God clearly reminded us that He
+    //         does not call the equipped, but He equips the called. We had
+    //         countless valuable conversations with the Filipinos and saw many
+    //         people accept Jesus into their hearts on this trip. It was an
+    //         amazing experience living at What The Bible Says Church and spending
+    //         so much time with the members. It was eye opening for us to see how
+    //         missional the church members are to their hometown and to see how
+    //         willing they are to preach the gospel to their neighbors.
+    //       </p>
+    //     </div>
+    //   )
+    // },
+    // {
+    //   name: 'Sooyoung Park (Not done)',
+    //   role: 'Role',
+    //   imgSrc: require('../images/staff/sooyoung_park.jpg'),
+    //   fellowship: 'Presence Livingwater',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         Before leaving for the trip, we prayed to God that He would show us
+    //         how He can work through His sons and daughters if they are obedient
+    //         and put full trust in Him. There were many times throughout this
+    //         trip where we felt like we were not equipped enough to carry out a
+    //         certain evangelical task (teaching classrooms, jail ministry,
+    //         drug/rehab center ministry), but God clearly reminded us that He
+    //         does not call the equipped, but He equips the called. We had
+    //         countless valuable conversations with the Filipinos and saw many
+    //         people accept Jesus into their hearts on this trip. It was an
+    //         amazing experience living at What The Bible Says Church and spending
+    //         so much time with the members. It was eye opening for us to see how
+    //         missional the church members are to their hometown and to see how
+    //         willing they are to preach the gospel to their neighbors.
+    //       </p>
+    //     </div>
+    //   )
+    // }
   ]
 };
 
