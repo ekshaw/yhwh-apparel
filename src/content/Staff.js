c5ec5f41081cb0720e579c052fb0e482c76ac51a
Peter Thompson <peter.thompson127@gmail.com>
2019-08-05 22:56:04 -0700

    added Tai-In's blurb

--- src/content/Staff.js
+++ src/content/Staff.js
@@ -134,32 +134,25 @@ const staff = {
           </p>
         </div>
       )
+    },
+    {
+      name: 'Tai-In Chung',
+      role: 'Marketing',
+      imgSrc: require('../images/staff/tai-in_chung.jpg'),
+      fellowship: 'BCPC',
+      description: (
+        <div>
+          <p>
+            What up, y'all! I'm Tai in and I joined YHWH Fall 2018! I am a
+            senior student studying Media Studies and Theater. I really
+            appreciate how everyone in YHWH puts God above anything. It is
+            fascinating to see how YHWH grows every year. Outside of YHWH, I
+            like to spend time working on theatrical shows, baking some cookies,
+            and painting my nails.
+          </p>
+        </div>
+      )
     }
-    // {
-    //   name: 'Tai-In Chung (Not done)',
-    //   role: 'Role',
-    //   imgSrc: require('../images/staff/tai-in_chung.jpg'),
-    //   fellowship: 'Fellowship Name',
-    //   description: (
-    //     <div>
-    //       <p>
-    //         Before leaving for the trip, we prayed to God that He would show us
-    //         how He can work through His sons and daughters if they are obedient
-    //         and put full trust in Him. There were many times throughout this
-    //         trip where we felt like we were not equipped enough to carry out a
-    //         certain evangelical task (teaching classrooms, jail ministry,
-    //         drug/rehab center ministry), but God clearly reminded us that He
-    //         does not call the equipped, but He equips the called. We had
-    //         countless valuable conversations with the Filipinos and saw many
-    //         people accept Jesus into their hearts on this trip. It was an
-    //         amazing experience living at What The Bible Says Church and spending
-    //         so much time with the members. It was eye opening for us to see how
-    //         missional the church members are to their hometown and to see how
-    //         willing they are to preach the gospel to their neighbors.
-    //       </p>
-    //     </div>
-    //   )
-    // },
     // {
     //   name: 'Sooyoung Park (Not done)',
     //   role: 'Role',
