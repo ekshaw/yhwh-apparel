78f68c84d85fea6ceee51b14ddf6fd7c62bc6920
Robb Matthew Flores <robbmatthew.flores@berkeley.edu>
2019-12-03 17:14:50 -0800

    unedited pictures added

--- src/content/Staff.js
+++ src/content/Staff.js
@@ -11,164 +11,177 @@ const staff = {
     {
       name: 'Hannah Lee',
       role: 'CEO / Manager',
-      imgSrc: require('../images/staff/hannah_lee.jpg'),
+      imgSrc: require('../images/staff/hannah.jpg'),
       fellowship: 'Presence Livingwater',
       description: (
         <div>
           <p>
-            "Hi, I'm Hannah and a senior at Cal. I joined YHWH my freshman year
-            and have been loving every season of it. I love binging sitcoms,
-            funny puppy videos, and learning random facts from the Internet.
-            Hope you are also encouraged by YHWH!"
-          </p>
-        </div>
-      )
-    },
-    {
-      name: 'Joseph Kim',
-      role: 'Design',
-      imgSrc: require('../images/staff/joseph_kim.jpg'),
-      fellowship: 'Passion Livingwater',
-      description: (
-        <div>
-          <p>
-            Joseph Kim has been a designer for YHWH since Spring 2018. He has a
-            strong passion for helping Christians and non believers alike grow
-            deeper in their knowledge of and ultimately love for God. Joseph
-            joined YHWH to use design to communicate themes of God's character
-            and plan of salvation through Jesus. His hope is that through YHWH
-            Apparel's products, Christians would not only be bold in their faith
-            through their outer appearance, but also equipped with Scripture in
-            their hearts to better explain and live out the Gospel.
+            "Hi, I'm Hannah and I'm a senior. I've been in YHWH since I was a wee freshman in 2016.
+            Invited by a junior sister, I found YHWH to be a safe, fun, and supportive environment
+            to explore my passions and creativity. YHWH also became a family to me, through every
+            season of new people; it was a place where brothers and sisters have actively listened,
+            encouraged, and loved me in my faith, especially when times are rocky. I love growing
+            with these people every year and drawing near to God together. In my free time, I enjoy
+            going on adventures with friends, trying new food, and reading fun books! "
           </p>
         </div>
       )
     },
     {
       name: 'Lillian Du',
-      role: 'Operations',
-      imgSrc: require('../images/staff/lillian_du.jpg'),
+      role: 'Operations / Web',
+      imgSrc: require('../images/staff/lilian_du.jpg'),
       fellowship: 'Presence Livingwater',
       description: (
         <div>
           <p>
-            "Hello! I'm a senior in Presence and I've been a part of YHWH's
-            operations team since Fall '17. I learn something new every semester
-            working with YHWH and I'm really excited to see how the team will
-            grow in the coming semesters!"
+            "Hello! I'm a senior in Presence.I’ve been a part of YHWH’s operations
+            team since my sophomore year at Berkeley. Being on the team has been a
+            way for me to express my faith in an open and tangible way and I learn
+            something new every semester working with YHWH. I’m looking forwards to
+            seeing how the team will grow in the future and am really excited to
+            share our products with everyone!"
           </p>
         </div>
       )
     },
-    {
-      name: 'Millie Ma',
-      role: 'Community Outreach',
-      imgSrc: require('../images/staff/millie_ma.jpg'),
-      fellowship: 'AACF',
-      description: (
-        <div>
-          <p>
-            Hi friends! My name’s Millie and I’m a recent cal grad who’s still
-            not ready to leave Berkeley yet. I’m an avid dogspotter, boba
-            drinker, and photo taker. I joined YHWH this past year and it’s been
-            a fun and blessed journey, very excited to see what’s to come!
-          </p>
-        </div>
-      )
-    },
-    {
-      name: 'Joseph Park',
-      role: 'Community Outreach',
-      imgSrc: require('../images/staff/joseph_park.jpg'),
-      fellowship: 'Acts 2 Fellowship',
-      description: (
-        <div>
-          <p>
-            "Hey everyone! I'm Joseph, and I'm a senior studying MCB and Data
-            Science. I've been a part of YHWH for one year now, and I really
-            enjoy that I get to represent what we're passionate about with the
-            team. Outside of YHWH and school, I really enjoy fellowshipping with
-            my friends, playing and watching soccer, and eating!"
-          </p>
-        </div>
-      )
-    },
+    // {
+    //   name: 'Millie Ma',
+    //   role: 'Community Outreach',
+    //   imgSrc: require('../images/staff/millie_ma.jpg'),
+    //   fellowship: 'Livingwater Young Adult',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         Hi friends! My name’s Millie and I’m a recent cal grad who’s still
+    //         not ready to leave Berkeley yet. I’m an avid dogspotter, boba
+    //         drinker, and photo taker. I joined YHWH this past year and it’s been
+    //         a fun and blessed journey, very excited to see what’s to come!
+    //       </p>
+    //     </div>
+    //   )
+    // },
     {
       name: 'Nicole Lee',
       role: 'Marketing',
-      imgSrc: require('../images/staff/nicole_lee.jpg'),
-      fellowship: '',
+      imgSrc: require('../images/staff/nicole.jpg'),
+      fellowship: 'RUF',
       description: (
         <div>
           <p>
-            "Hi! My name is Nicole Lee and I am member of YHWH’s marketing team.
-            I am a junior at Cal studying Media Studies, hoping to honor Christ
-            in all that I do."
+            "Hi! My name is Nicole Lee and I am currently a junior majoring in
+            Media Studies. I am a part of Reformed University Fellowship (RUF) at
+            Cal. I have been in YHWH for 2 years now and love that I am able to
+            share my faith and do meaningful work alongside some really cool people.
+            In my free time I enjoy cooking, writing, and binge watching shows
+            from my childhood!."
           </p>
         </div>
       )
     },
     {
       name: 'Peter Thompson',
-      role: 'Web',
-      imgSrc: require('../images/staff/peter_thompson.jpg'),
+      role: 'Operations / Web',
+      imgSrc: require('../images/staff/peter.jpg'),
       fellowship: 'Passion Livingwater',
       description: (
         <div>
           <p>
-            "Hey, I'm Peter, and I'm currently a junior at Cal studying Computer
-            Science. I joined YHWH my sophomore year, and being a part of this
-            team and community has been such a blast. It's been so amazing to
-            see how we've been able to carry out God's mission through our
-            apparel, and I'm so excited to see what lies ahead. Apart from YHWH,
-            I love to play basketball, go snowboarding, and play guitar."
+            "Hey, I’m Peter, and I’m currently a junior at Cal studying Computer
+            Science. I joined YHWH my sophomore year, and being a part of this team
+            and community has been such a blessing! It’s been so amazing to see how
+            we’ve been able to carry out God’s mission through our apparel, and I’m
+            so excited to see how He’s going to continue to grow and work through us.
+            Apart from YHWH, I love to play basketball, go snowboarding, and play guitar."
+          </p>
+        </div>
+      )
+    },
+    // {
+    //   name: 'Angela Ea',
+    //   role: 'Design',
+    //   imgSrc: require('../images/staff/tai-in_chung.jpg'),
+    //   fellowship: 'Livingwater Passion',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         ""
+    //       </p>
+    //     </div>
+    //   )
+    // },
+    // {
+    //   name: 'Sooyoung Park',
+    //   role: 'Marketing',
+    //   imgSrc: require('../images/staff/tai-in_chung.jpg'),
+    //   fellowship: 'Livingwater Presence',
+    //   description: (
+    //     <div>
+    //       <p>
+    //         ""
+    //       </p>
+    //     </div>
+    //   )
+    // },
+    {
+      name: 'Matt Flores',
+      role: 'Operations / Web',
+      imgSrc: require('../images/staff/matt_flores.jpg'),
+      fellowship: 'Livingwater Passion',
+      description: (
+        <div>
+          <p>
+            "Hi, I'm Matt and I'm a sophomore at Cal studying Cognitive Science.
+            This is my first semester in YHWH and its been such a blessing and
+            privilege to be apart of a team so dedicated to making a product that
+            glorifies God. This team has showed me how I can use my skills and
+            passions (in logistics, communication, and web design) to bring honor
+            to God, and I am so grateful to have the opportunity to play a small
+            part in His big plan. I joined because I felt so compelled to start
+            conversations with my clothing and my appearance, and this semester has
+            showed me how God works powerfully through the smallest things, whether
+            it be a necklace or a jacket. In my free time,  I love dancing,
+            playing volleyball, and exploring design."
           </p>
         </div>
       )
     },
     {
-      name: 'Tai-In Chung',
-      role: 'Marketing',
-      imgSrc: require('../images/staff/tai-in_chung.jpg'),
-      fellowship: 'BCPC',
+      name: 'Emily Shaw',
+      role: 'Design',
+      imgSrc: require('../images/staff/emily_shaw.jpg'),
+      fellowship: '',
       description: (
         <div>
           <p>
-            What up, y'all! I'm Tai in and I joined YHWH Fall 2018! I am a
-            senior student studying Media Studies and Theater. I really
-            appreciate how everyone in YHWH puts God above anything. It is
-            fascinating to see how YHWH grows every year. Outside of YHWH, I
-            like to spend time working on theatrical shows, baking some cookies,
-            and painting my nails.
+            "What's up! I'm Emily and I'm a sophomore at Cal.
+            This is my first semester in YHWH and I feel so blessed to be able work
+            and grow alongside an amazing team to create designs and clothing that
+            spark conversations about God. In my free time, I love hiking, swimming,
+            cooking, and drinking boba."
+          </p>
+        </div>
+      )
+    },
+    {
+      name: 'Aaron Wu',
+      role: 'Design',
+      imgSrc: require('../images/staff/aaron_wu.jpg'),
+      fellowship: 'Livingwater Passion',
+      description: (
+        <div>
+          <p>
+            "Hi I'm Aaron and I'm a freshman at Berkeley. I joined YHWH this semester
+            because I wanted to be part of a creative
+            outlet for a higher purpose. As a member of the Design team, being in
+            YHWH has encouraged me to think about my faith abstractly and artistically,
+            and to translate that into biblical, God-breathed visuals. Besides design,
+            I like film photography, hanging out with friends, reading the news, and
+            exercising!"
           </p>
         </div>
       )
     }
-    // {
-    //   name: 'Sooyoung Park (Not done)',
-    //   role: 'Role',
-    //   imgSrc: require('../images/staff/sooyoung_park.jpg'),
-    //   fellowship: 'Presence Livingwater',
-    //   description: (
-    //     <div>
-    //       <p>
-    //         Before leaving for the trip, we prayed to God that He would show us
-    //         how He can work through His sons and daughters if they are obedient
-    //         and put full trust in Him. There were many times throughout this
-    //         trip where we felt like we were not equipped enough to carry out a
-    //         certain evangelical task (teaching classrooms, jail ministry,
-    //         drug/rehab center ministry), but God clearly reminded us that He
-    //         does not call the equipped, but He equips the called. We had
-    //         countless valuable conversations with the Filipinos and saw many
-    //         people accept Jesus into their hearts on this trip. It was an
-    //         amazing experience living at What The Bible Says Church and spending
-    //         so much time with the members. It was eye opening for us to see how
-    //         missional the church members are to their hometown and to see how
-    //         willing they are to preach the gospel to their neighbors.
-    //       </p>
-    //     </div>
-    //   )
-    // }
   ]
 };
 
