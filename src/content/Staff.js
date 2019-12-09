90eba4fb4890136e04fbeffcaafbdb9cbb3eaf87
Peter Thompson <peter.thompson127@gmail.com>
2019-12-09 15:24:14 -0800

    updated staff page to add angela and millie | fixed design styling

--- src/content/Staff.js
+++ src/content/Staff.js
@@ -11,18 +11,20 @@ const staff = {
     {
       name: 'Hannah Lee',
       role: 'CEO / Manager',
-      imgSrc: require('../images/staff/hannah.jpg'),
-      fellowship: 'Presence Livingwater',
+      imgSrc: require('../images/staff/hannah_lee.jpg'),
+      fellowship: 'Livingwater Presence',
       description: (
         <div>
           <p>
-            "Hi, I'm Hannah and I'm a senior. I've been in YHWH since I was a wee freshman in 2016.
-            Invited by a junior sister, I found YHWH to be a safe, fun, and supportive environment
-            to explore my passions and creativity. YHWH also became a family to me, through every
-            season of new people; it was a place where brothers and sisters have actively listened,
-            encouraged, and loved me in my faith, especially when times are rocky. I love growing
-            with these people every year and drawing near to God together. In my free time, I enjoy
-            going on adventures with friends, trying new food, and reading fun books! "
+            "Hi, I'm Hannah and I'm a senior. I've been in YHWH since I was a
+            wee freshman in 2016. Invited by a junior sister, I found YHWH to be
+            a safe, fun, and supportive environment to explore my passions and
+            creativity. YHWH also became a family to me, through every season of
+            new people; it was a place where brothers and sisters have actively
+            listened, encouraged, and loved me in my faith, especially when
+            times are rocky. I love growing with these people every year and
+            drawing near to God together. In my free time, I enjoy going on
+            adventures with friends, trying new food, and reading fun books!"
           </p>
         </div>
       )
@@ -30,51 +32,54 @@ const staff = {
     {
       name: 'Lillian Du',
       role: 'Operations / Web',
-      imgSrc: require('../images/staff/lilian_du.jpg'),
-      fellowship: 'Presence Livingwater',
+      imgSrc: require('../images/staff/lillian_du.jpg'),
+      fellowship: 'Livingwater Presence',
       description: (
         <div>
           <p>
-            "Hello! I'm a senior in Presence.I’ve been a part of YHWH’s operations
-            team since my sophomore year at Berkeley. Being on the team has been a
-            way for me to express my faith in an open and tangible way and I learn
-            something new every semester working with YHWH. I’m looking forwards to
-            seeing how the team will grow in the future and am really excited to
-            share our products with everyone!"
+            "Hello! I'm a senior in Presence.I’ve been a part of YHWH’s
+            operations team since my sophomore year at Berkeley. Being on the
+            team has been a way for me to express my faith in an open and
+            tangible way and I learn something new every semester working with
+            YHWH. I’m looking forwards to seeing how the team will grow in the
+            future and am really excited to share our products with everyone!"
           </p>
         </div>
       )
     },
-    // {
-    //   name: 'Millie Ma',
-    //   role: 'Community Outreach',
-    //   imgSrc: require('../images/staff/millie_ma.jpg'),
-    //   fellowship: 'Livingwater Young Adult',
-    //   description: (
-    //     <div>
-    //       <p>
-    //         Hi friends! My name’s Millie and I’m a recent cal grad who’s still
-    //         not ready to leave Berkeley yet. I’m an avid dogspotter, boba
-    //         drinker, and photo taker. I joined YHWH this past year and it’s been
-    //         a fun and blessed journey, very excited to see what’s to come!
-    //       </p>
-    //     </div>
-    //   )
-    // },
+    {
+      name: 'Millie Ma',
+      role: 'Community Outreach',
+      imgSrc: require('../images/staff/millie_ma.jpg'),
+      fellowship: 'AACF',
+      description: (
+        <div>
+          <p>
+            "I've been in YHWH since last fall, and originally I joined because
+            I was inspired by the people I saw who wore YHWH apparel on campus.
+            Being apart of this team has made me be more courageous in my faith,
+            as well as encouraged me in seeking the Lord in all areas of my
+            life. It's been a place where I've been really challenged to think
+            deeply about who God is and He means to me. Hobbies include
+            dogspotting, picture-taking, ice cream eating, and kdrama watching."
+          </p>
+        </div>
+      )
+    },
     {
       name: 'Nicole Lee',
       role: 'Marketing',
-      imgSrc: require('../images/staff/nicole.jpg'),
-      fellowship: 'RUF',
+      imgSrc: require('../images/staff/nicole_lee.jpg'),
+      fellowship: 'Reformed University Fellowship',
       description: (
         <div>
           <p>
             "Hi! My name is Nicole Lee and I am currently a junior majoring in
-            Media Studies. I am a part of Reformed University Fellowship (RUF) at
-            Cal. I have been in YHWH for 2 years now and love that I am able to
-            share my faith and do meaningful work alongside some really cool people.
-            In my free time I enjoy cooking, writing, and binge watching shows
-            from my childhood!."
+            Media Studies. I am a part of Reformed University Fellowship (RUF)
+            at Cal. I have been in YHWH for 2 years now and love that I am able
+            to share my faith and do meaningful work alongside some really cool
+            people. In my free time I enjoy cooking, writing, and binge watching
+            shows from my childhood!."
           </p>
         </div>
       )
@@ -82,49 +87,69 @@ const staff = {
     {
       name: 'Peter Thompson',
       role: 'Operations / Web',
-      imgSrc: require('../images/staff/peter.jpg'),
-      fellowship: 'Passion Livingwater',
+      imgSrc: require('../images/staff/peter_thompson.jpg'),
+      fellowship: 'Livingwater Passion',
       description: (
         <div>
           <p>
             "Hey, I’m Peter, and I’m currently a junior at Cal studying Computer
-            Science. I joined YHWH my sophomore year, and being a part of this team
-            and community has been such a blessing! It’s been so amazing to see how
-            we’ve been able to carry out God’s mission through our apparel, and I’m
-            so excited to see how He’s going to continue to grow and work through us.
-            Apart from YHWH, I love to play basketball, go snowboarding, and play guitar."
+            Science. I joined YHWH my sophomore year, and being a part of this
+            team and community has been such a blessing! It’s been so amazing to
+            see how we’ve been able to carry out God’s mission through our
+            apparel, and I’m so excited to see how He’s going to continue to
+            grow and work through us. Apart from YHWH, I love to play
+            basketball, go snowboarding, and play guitar."
           </p>
         </div>
       )
     },
-    // {
-    //   name: 'Angela Ea',
-    //   role: 'Design',
-    //   imgSrc: require('../images/staff/tai-in_chung.jpg'),
-    //   fellowship: 'Livingwater Passion',
-    //   description: (
-    //     <div>
-    //       <p>
-    //         ""
-    //       </p>
-    //     </div>
-    //   )
-    // },
+    {
+      name: 'Angela Ea',
+      role: 'Design',
+      imgSrc: require('../images/staff/angela-ea.jpg'),
+      fellowship: 'Livingwater Passion',
+      description: (
+        <div>
+          <p>
+            "I have long been a fan of YHWH’s cozy apparel and liked how boldly
+            “Christian” the clothes were. I never pictured myself joining until
+            I saw a pretty set of photos get posted on YHWH’s Instagram. I
+            recognized a non-Christian friend of mine modeling the Adonai
+            sweater, and my jaw dropped. Seeing my friend in that photo made me
+            remember our greater goal as Christians to spread God’s Kingdom. I
+            was reminded how clothing can send a message and start
+            conversations. Not only that, but the aesthetics of the photo
+            triggered my old love of taking photos. I was eager to channel my
+            passion for photography into something productive and God
+            glorifying. By default, I was put on the design team. I am a bit
+            clueless when it comes to graphic design, and did not think I would
+            be helping too much in designing our products. But God has shown me
+            countless times how He uses weak people to make great things happen.
+            My time at YHWH has already been such a blessing. There is so much
+            symbolism and meaning that goes into our designs from the color of
+            the fabric or why something might be embroidered. Each piece of
+            clothing has so much thought put into it, and I am so proud to be
+            part of such a hard-working team!"
+          </p>
+        </div>
+      )
+    },
     {
       name: 'Sooyoung Park',
       role: 'Marketing',
-      imgSrc: require('../images/staff/sooyoung.jpg'),
+      imgSrc: require('../images/staff/sooyoung_park.jpg'),
       fellowship: 'Livingwater Presence',
       description: (
         <div>
           <p>
             "I've been in YHWH since the beginning of my sophomore year. Through
-            praying and sharing life with one another, it's been amazing not only
-            hearing about how God has been working through each and every one of our
-            lives, but also witnessing my personal growth in my faith. It's been such
-            a blessing to be a part of a team that just loves God so wholeheartedly.
-            Outside of YHWH and classes, I enjoy bullet journaling, playing basketball
-            and piano, and going on food adventures!"
+            praying and sharing life with one another, it's been amazing not
+            only hearing about how God has been working through each and every
+            one of our lives, but also witnessing my personal growth in my
+            faith. It's been such a blessing to be a part of a team that just
+            loves God so wholeheartedly. Outside of YHWH and classes, I enjoy
+            bullet journaling, playing basketball and piano, and going on food
+            adventures!"
           </p>
         </div>
       )
@@ -139,15 +164,15 @@ const staff = {
           <p>
             "Hi, I'm Matt and I'm a sophomore at Cal studying Cognitive Science.
             This is my first semester in YHWH and its been such a blessing and
-            privilege to be apart of a team so dedicated to making a product that
-            glorifies God. This team has showed me how I can use my skills and
-            passions (in logistics, communication, and web design) to bring honor
-            to God, and I am so grateful to have the opportunity to play a small
-            part in His big plan. I joined because I felt so compelled to start
-            conversations with my clothing and my appearance, and this semester has
-            showed me how God works powerfully through the smallest things, whether
-            it be a necklace or a jacket. In my free time,  I love dancing,
-            playing volleyball, and exploring design."
+            privilege to be apart of a team so dedicated to making a product
+            that glorifies God. This team has showed me how I can use my skills
+            and passions (in logistics, communication, and web design) to bring
+            honor to God, and I am so grateful to have the opportunity to play a
+            small part in His big plan. I joined because I felt so compelled to
+            start conversations with my clothing and my appearance, and this
+            semester has showed me how God works powerfully through the smallest
+            things, whether it be a necklace or a jacket. In my free time, I
+            love dancing, playing volleyball, and exploring design."
           </p>
         </div>
       )
@@ -160,10 +185,10 @@ const staff = {
       description: (
         <div>
           <p>
-            "What's up! I'm Emily and I'm a sophomore at Cal.
-            This is my first semester in YHWH and I feel so blessed to be able work
-            and grow alongside an amazing team to create designs and clothing that
-            spark conversations about God. In my free time, I love hiking, swimming,
+            "What's up! I'm Emily and I'm a sophomore at Cal. This is my first
+            semester in YHWH and I feel so blessed to be able work and grow
+            alongside an amazing team to create designs and clothing that spark
+            conversations about God. In my free time, I love hiking, swimming,
             cooking, and drinking boba."
           </p>
         </div>
@@ -177,13 +202,13 @@ const staff = {
       description: (
         <div>
           <p>
-            "Hi I'm Aaron and I'm a freshman at Berkeley. I joined YHWH this semester
-            because I wanted to be part of a creative
-            outlet for a higher purpose. As a member of the Design team, being in
-            YHWH has encouraged me to think about my faith abstractly and artistically,
-            and to translate that into biblical, God-breathed visuals. Besides design,
-            I like film photography, hanging out with friends, reading the news, and
-            exercising!"
+            "Hi I'm Aaron and I'm a freshman at Berkeley. I joined YHWH this
+            semester because I wanted to be part of a creative outlet for a
+            higher purpose. As a member of the Design team, being in YHWH has
+            encouraged me to think about my faith abstractly and artistically,
+            and to translate that into biblical, God-breathed visuals. Besides
+            design, I like film photography, hanging out with friends, reading
+            the news, and exercising!"
           </p>
         </div>
       )
