cd90258e3d45a73cfbdcfbd257b4687dc8470a72
Peter Thompson <peter.thompson127@gmail.com>
2020-10-31 09:02:35 -0700

    add sliding transition in mobile navigation

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -19,20 +19,20 @@ toggle between hiding and showing the dropdown content */
       hide_menu: !this.state.hide_menu,
       background_index: (this.state.background_index + 1) % 2,
     });
-    console.log(this.state.background_index);
-    // document.getElementById('myDropdown').classList.toggle('show');
-    // if (document.getElementById('menubar').style.backgroundColor == '') {
-    //   document.getElementById('menubar').style.backgroundColor = '#D3CEC7';
-    // } else {
-    //   document.getElementById('menubar').style.backgroundColor = '';
-    // }
   };
+
+  onLogoClick = () => {
+    this.setState({
+      hide_menu: true,
+      background_index: 0,
+    });
+  }
 
   render() {
     return (
       <div>
         <div className='mobile-logo'>
-          <Link to='/'>
+          <Link to='/' onClick={this.onLogoClick}>
             <img
               src={require('../images/logo.png')}
               alt='Logo'
@@ -43,15 +43,20 @@ toggle between hiding and showing the dropdown content */
         <div
           onClick={this.click}
           className={'hamburger-icon' + (this.state.hide_menu ? '' : ' fixed')}
+          id='menu-toggle'
         >
-          <img
+          {/* <img
             src={require('../images/hamburger_menu.svg')}
             id='hamburger-icon'
-          ></img>
+          ></img> */}
+          <input type="checkbox" checked={!this.state.hide_menu} />
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <div
           id='menubar'
-          className={this.state.hide_menu ? 'hide' : ''}
+          className={this.state.hide_menu ? 'hide' : 'show'}
           style={{
             backgroundColor: this.state.curr_background[
               this.state.background_index
@@ -59,23 +64,23 @@ toggle between hiding and showing the dropdown content */
           }}
         >
           <div className='menu-items-holder'>
-            <div id='myDropdown' className='menubar-text'>
-              <Link to='/about' className='menubar-text'>
+            <div id='myDropdown' className={'menubar-text' + (this.state.hide_menu ? ' hide' : ' show')}>
+              <Link to='/about' className='menubar-text' onClick={this.click}>
                 <h4>ABOUT YHWH</h4>
               </Link>
-              <Link to='/staff' className='menubar-text'>
+              <Link to='/staff' className='menubar-text' onClick={this.click}>
                 <h4>OUR TEAM</h4>
               </Link>
-              <Link to='/designs' className='menubar-text'>
+              <Link to='/designs' className='menubar-text' onClick={this.click}>
                 <h4>DESIGNS</h4>
               </Link>
-              {/* <Link to='/shop' className='menubar-text'>
+              {/* <Link to='/shop' className='menubar-text' onClick={this.click}>
                 <h4>SHOP</h4>
               </Link> */}
               <a href='https://yhwhapparel.storenvy.com/products' className='menubar-text'>
                 <h4 className='mobile-navlink'>SHOP</h4>
               </a>
-              <Link to='/partnerships' className='menubar-text'>
+              <Link to='/partnerships' className='menubar-text' onClick={this.click}>
                 <h4>PARTNERSHIPS</h4>
               </Link>
             </div>
