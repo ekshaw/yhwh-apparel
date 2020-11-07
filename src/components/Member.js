14887f41b423fdc2eaa607c09abcc4f154b9b51d
Emily Shaw <ekshaw@berkeley.edu>
2020-11-06 18:11:10 -0800

    finished staff pg update

---
+++ src/components/Member.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Member.css';
+
+const Member = ({ odd, member: { imgSrc, name, role, description } }) => {
+  return (
+    <div className='member-main-container'>
+      <div className={'member-' + (odd == 1 ? 'odd' : 'even')}>
+        <div className='member-image'>
+          <img src={imgSrc} alt='' />
+        </div>
+        <div className='member-description-container'>
+          <div>
+            <div className='member-name'>
+              <h2>{name}</h2>
+            </div>
+            <div className='member-role'>{role}</div>
+            <div className='member-description'>{description}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Member;
