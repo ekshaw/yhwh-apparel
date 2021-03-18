02be33cb164d37348c98b6ae28d52f010673c389
ekshaw <48424279+ekshaw@users.noreply.github.com>
2021-03-17 22:59:12 -0700

    Revert "Emily/subscription form"

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,135 +1,28 @@
-import React, { Component } from 'react';
-import FlashMessage from 'react-flash-message';
+import React from 'react';
 
-class SubscribeForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fname: '',
-      lname: '',
-      email: '',
-      flashMsgTxt: '',
-      showFlasgMsg: false,
-      buttonText: 'SUBMIT'
-    };
-  }
-
-  onSubscriptionButtonClick = async e => {
-    const { fname, lname, email } = this.state;
-    const { hostname: location } = window.location;
-
-    await this.setState({
-      showFlashMsg: false,
-      buttonText: '...loading'
-    });
-
-    if (!fname || !lname || !email) {
-      this.setState({
-        flashMsgTxt: 'Please check that your inputs are valid!',
-        showFlashMsg: true,
-        buttonText: 'SUBMIT'
-      });
-    } else {
-      e.preventDefault();
-      const response = await fetch('/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ first_name: fname, last_name: lname, email: email })
-      });
-      const body = await response.text();
-
-      if (body === 'OK') {
-        this.setState({
-          flashMsgTxt: "You're subscribed!",
-          showFlashMsg: true,
-          fname: '',
-          lname: '',
-          email: '',
-          buttonText: 'SUBMIT'
-        });
-      } else {
-        const bodyResponse = JSON.parse(body).title;
-        if (bodyResponse === 'Member Exists') {
-          this.setState({
-            flashMsgTxt: "You're already subscribed!",
-            showFlashMsg: true,
-            fname: '',
-            lname: '',
-            email: '',
-            buttonText: 'SUBMIT'
-          });
-        } else {
-          this.setState({
-            flashMsgTxt: 'Something went wrong! Try again?',
-            showFlashMsg: true,
-            buttonText: 'SUBMIT'
-          });
-        }
-      }
-    }
-  };
-
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  render() {
-    return (
-      <div className='form-input-container'>
-        <div className='form-field first-name-field'>
-          <h4>FIRST NAME:</h4>
-          <label>
-            <input
-              type='text'
-              id='first-name-input'
-              name='fname'
-              value={this.state.fname}
-              required
-              onChange={e => this.handleChange(e)}
-            />
-          </label>
-        </div>
-        <div className='form-field last-name-field'>
-          <h4>LAST NAME:</h4>
-          <label>
-            <input
-              type='text'
-              id='last-name-input'
-              name='lname'
-              value={this.state.lname}
-              required
-              onChange={e => this.handleChange(e)}
-            />
-          </label>
-        </div>
-        <div className='form-field email-field'>
-          <h4>EMAIL:</h4>
-          <label>
-            <input
-              type='text'
-              id='email-input'
-              name='email'
-              value={this.state.email}
-              required
-              onChange={e => this.handleChange(e)}
-            />
-          </label>
-        </div>
-        <div className='subscribe_form-submit-btn' onClick={this.onSubscriptionButtonClick}>
-          <h4>{this.state.buttonText}</h4>
-        </div>
-        {this.state.showFlashMsg && (
-          <div>
-            <FlashMessage duration={5000}>
-              <h4>{this.state.flashMsgTxt}</h4>
-            </FlashMessage>
-          </div>
-        )}
+const SubscribeForm = props => {
+  return (
+    <div className='form-input-container'>
+      <div className='form-field first-name-field'>
+        <h4>FIRST NAME:</h4>
+        <label>
+          <input type='text' id='first-name-input' />
+        </label>
       </div>
-    );
-  }
-}
+      <div className='form-field last-name-field'>
+        <h4>LAST NAME:</h4>
+        <label>
+          <input type='text' id='last-name-input' />
+        </label>
+      </div>
+      <div className='form-field email-field'>
+        <h4>EMAIL:</h4>
+        <label>
+          <input type='text' id='email-input' />
+        </label>
+      </div>
+    </div>
+  );
+};
 
 export default SubscribeForm;
