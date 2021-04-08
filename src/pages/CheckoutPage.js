9ad1677f35c05512ac7eb7084932dae4fe842d72
Peter Thompson <peter.thompson127@gmail.com>
2021-04-08 01:31:33 -0700

    add cookies to keep track of cart

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -5,90 +5,133 @@ import Products from '../content/Products';
 import { useCookies } from 'react-cookie';
 import Cookies from 'universal-cookie';
 
-const CheckoutPage = props => {
+const CheckoutPage = (props) => {
   const cookies = new Cookies();
+  const cookieName = 'sandyCheeks';
 
-  const [cart, setCart] = useState([]);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [cookie, setCookie] = useState({});
 
-  cookies.set('myBagTotal', cartTotal, { path: '/' });
-  console.log(cookies.get('myBagTotal'));
+  useEffect(() => {}, [cookie]);
 
-  // updates total & cookie if cart changes
-  useEffect(() => {
-    total();
-    cookies.set('myBagTotal', cartTotal, { path: '/' });
-    createCookie('myBag', JSON.stringify(cart));
-  }, [cart]);
+  // const [cart, setCart] = useState([]);
+  // const [cartTotal, setCartTotal] = useState(0);
 
-  // calculates total
-  const total = () => {
-    let totalVal = 0;
-    for (let i = 0; i < cart.length; i++) {
-      totalVal += cart[i].price;
-    }
-    setCartTotal(totalVal);
-  };
+  // // cookies.set('myBagTotal', cartTotal, { path: '/' });
+  // cookies.set('myBag', JSON.stringify(cart), { path: '/' });
+  // // console.log(cookies.get('myBagTotal'));
 
-  // adds item to cart
-  const addToCart = el => {
-    setCart([...cart, el]);
-  };
+  // // updates total & cookie if cart changes
+  // useEffect(() => {
+  //   console.log(cart);
+  //   total();
+  //   // cookies.set('myBagTotal', cartTotal, { path: '/' });
+  //   cookies.set('myBag', JSON.stringify(cart), { path: '/' });
+  // }, [cart]);
+
+  // // calculates total
+  // const total = () => {
+  //   let totalVal = 0;
+  //   for (let i = 0; i < cart.length; i++) {
+  //     totalVal += cart[i].price;
+  //   }
+  //   setCartTotal(totalVal);
+  // };
+
+  // // adds item to cart
+  // const addToCart = (el) => {
+  //   setCart([...cart, el]);
+  // };
 
   // removes item from cart (need to figure out how to deal with duplicates!)
-  const removeFromCart = el => {
-    let hardCopy = [...cart];
-    hardCopy = hardCopy.filter(cartItem => cartItem.title !== el.title);
-    setCart(hardCopy);
+  const removeFromCart = (el) => {
+    let hardCopy = [...cookies.get(cookieName)];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.title !== el.title);
+    cookies.set(cookieName, JSON.stringify(hardCopy));
+    setCookie(cookies.get(cookieName));
+  };
+
+  const addToCart = (el) => {
+    const { title, price } = el;
+    const item = {
+      title: title,
+      price: price,
+    };
+    let cookie = cookies.get(cookieName);
+    if (cookie) {
+      const cart = objectToArray(cookie);
+      cart.push(item);
+      cookies.set(cookieName, JSON.stringify(cart), {
+        path: '/',
+      });
+    } else {
+      cookies.set(cookieName, JSON.stringify([item]), { path: '/' });
+    }
+    cookie = cookies.get(cookieName);
+    setCookie(cookie);
+  };
+
+  const objectToArray = (object) => {
+    return Object.keys(object).map((key) => object[key]);
   };
 
   // list of products (can delete later once we connect addtoCart to product pages)
-  const listItems = Products.map(el => (
+  const listItems = Products.map((el) => (
     <div key={el.name}>
       {`${el.title}: $${el.price}`}
       <input type='submit' value='add to cart' onClick={() => addToCart(el)} />
     </div>
   ));
 
-  // list of cart items in checkout page
-  const cartItems = cart.map(el => (
-    <div key={el.name}>
-      <div className='checkout-item'>
-        <div className='checkout-item-img'>{el.images[0]}</div>
-        <div className='checkout-item-description'>
-          <div className='checkout-item-title'>
-            <h4>{`${el.title}`}</h4>
-          </div>
-          <div className='checkout-item-price'>
-            <h4>UNIT PRICE: {`$${el.price}`}</h4>
-          </div>
-        </div>
-        <div className='checkout-item-delete'>
-          <img
-            src={require('../images/shop/x-button.png')}
-            onClick={() => removeFromCart(el)}></img>
-        </div>
-      </div>
-    </div>
-  ));
-
-  // creates cookies (not updating?:"))
-  function createCookie(name, value, days) {
-    var expires;
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = ', expires=' + date.toGMTString();
-    } else {
-      expires = '';
+  const getProductImage = (title) => {
+    for (let i = 0; i < Products.length; i++) {
+      if (title === Products[i].title) {
+        return Products[i].images[0];
+      }
     }
-    console.log('>>>>>>>>>>>', value);
-    document.cookie = name + '=' + value + expires + ', path=/';
-  }
+  };
 
-  function eraseCookie(name) {
-    createCookie(name, '', -1);
-  }
+  // list of cart items in checkout page
+  const cartItems = cookies.get(cookieName)
+    ? objectToArray(cookies.get(cookieName)).map((el) => (
+        <div key={el.name}>
+          <div className='checkout-item'>
+            <div className='checkout-item-img'>{getProductImage(el.title)}</div>
+            <div className='checkout-item-description'>
+              <div className='checkout-item-title'>
+                <h4>{`${el.title}`}</h4>
+              </div>
+              <div className='checkout-item-price'>
+                <h4>UNIT PRICE: {`$${el.price}`}</h4>
+              </div>
+            </div>
+            <div className='checkout-item-delete'>
+              <img
+                src={require('../images/shop/x-button.png')}
+                onClick={() => removeFromCart(el)}
+              ></img>
+            </div>
+          </div>
+        </div>
+      ))
+    : null;
+
+  // // creates cookies (not updating?:"))
+  // function createCookie(name, value, days) {
+  //   var expires;
+  //   if (days) {
+  //     var date = new Date();
+  //     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  //     expires = ', expires=' + date.toGMTString();
+  //   } else {
+  //     expires = '';
+  //   }
+  //   console.log('>>>>>>>>>>>', value);
+  //   document.cookie = name + '=' + value + expires + ', path=/';
+  // }
+
+  // function eraseCookie(name) {
+  //   createCookie(name, '', -1);
+  // }
 
   return (
     <div className='checkout-main-container'>
@@ -119,7 +162,7 @@ const CheckoutPage = props => {
               </div>
               <div className='checkout-summary-subtotal'>
                 <h4>SUBTOTAL:</h4>
-                <h3>${cartTotal}</h3>
+                {/* <h3>${cartTotal}</h3> */}
                 <h5>Shipping and tax calculated at checkout.</h5>
               </div>
               <div className='checkout-summary-button'>
