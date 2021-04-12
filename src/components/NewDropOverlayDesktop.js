b238d56f69e236de139d87435e48cdd3a0d06969
Christie Lum <christielum@berkeley.edu>
2021-04-11 22:21:30 -0700

    started desktop popup

--- src/components/NewDropOverlayDesktop.js
+++ src/components/NewDropOverlayDesktop.js
@@ -22,13 +22,14 @@ class NewDropOverlayDesktop extends Component {
       <div className='new-drop-container'>
         <div className='new-drop-overlay' onClick={this.onXClick}></div>
         <div className='new-drop-content-container'>
-          <img className='new-drop-overlay-img' src={require('../images/home/assurance-drop.png')} />
+          <img className='new-drop-overlay-img' src={require('../images/home/offseason.png')} />
           <div className='new-drop-overlay-exit-btn' onClick={this.onXClick}>
             <h3>X</h3>
           </div>
-          <div className='new-drop-overlay-header'><h3>F/W '20 COLLECTION</h3></div>
+          <div className='new-drop-overlay-header'><h3>FALL 2021</h3></div>
           <div className='new-drop-overlay-header-underline'></div>
-          <div className='new-drop-overlay-title'><h1>Assurance.</h1></div>
+          <div className='new-drop-overlay-title'>Subscribe to us</div>
+          <div className='new-drop-overlay-subtitle'>We'd love to stay in touch with you!</div>
           <div className='new-drop-overlay-order-btn'>
             <a href='https://tinyurl.com/yhwhassurance'><h4>ORDER NOW</h4></a>
           </div>
