f3523a854ffbecadf93b65aa94745ce519414933
Christie Lum <christielum@berkeley.edu>
2021-04-11 23:10:41 -0700

    added the input form

--- src/components/NewDropOverlayDesktop.js
+++ src/components/NewDropOverlayDesktop.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FlashMessage from 'react-flash-message'
 import '../styles/NewDropOverlayDesktop.css';
 
 class NewDropOverlayDesktop extends Component {
@@ -7,7 +8,74 @@ class NewDropOverlayDesktop extends Component {
     super(props);
     this.state = {
       dropOverlayHidden: false,
+      fname: '',
+      lname: '',
+      email: '',
+      flashMsgTxt: '',
+      showFlasgMsg: false,
+      buttonText: 'SUBMIT'
     }
+  }
+
+  onSubscriptionButtonClick = async e => {
+    const { fname, lname, email } = this.state;
+    const { hostname: location } = window.location;
+
+    await this.setState({
+      showFlashMsg: false,
+      buttonText: '...loading'
+    });
+
+    if (!fname || !lname || !email) {
+      this.setState({
+        flashMsgTxt: 'Please check that your inputs are valid!',
+        showFlashMsg: true,
+        buttonText: 'SUBMIT'
+      });
+    } else {
+      e.preventDefault();
+      const response = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ first_name: fname, last_name: lname, email: email })
+      });
+      const body = await response.text();
+
+      if (body === 'OK') {
+        this.setState({
+          flashMsgTxt: "You're subscribed!",
+          showFlashMsg: true,
+          fname: '',
+          lname: '',
+          email: '',
+          buttonText: 'SUBMIT'
+        });
+      } else {
+        const bodyResponse = JSON.parse(body).title;
+        if (bodyResponse === 'Member Exists') {
+          this.setState({
+            flashMsgTxt: "You're already subscribed!",
+            showFlashMsg: true,
+            fname: '',
+            lname: '',
+            email: '',
+            buttonText: 'SUBMIT'
+          });
+        } else {
+          this.setState({
+            flashMsgTxt: 'Something went wrong! Try again?',
+            showFlashMsg: true,
+            buttonText: 'SUBMIT'
+          });
+        }
+      }
+    }
+  };
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onXClick = () => {
@@ -30,9 +98,72 @@ class NewDropOverlayDesktop extends Component {
           <div className='new-drop-overlay-header-underline'></div>
           <div className='new-drop-overlay-title'>Subscribe to us</div>
           <div className='new-drop-overlay-subtitle'>We'd love to stay in touch with you!</div>
-          <div className='new-drop-overlay-order-btn'>
-            <a href='https://tinyurl.com/yhwhassurance'><h4>ORDER NOW</h4></a>
+          <div className='new-drop-overlay-form-title'>Our Newsletter</div>
+          <div className="new-drop-overlay-form">
+            <div className='new-drop-field'>
+              <label>
+                <input
+                  class='new-drop-overlay-input'
+                  type='text'
+                  id='first-name-input'
+                  name='fname'
+                  value={this.state.fname}
+                  required
+                  placeholder='First Name'
+                  outline='none'
+                  onChange={e => this.handleChange(e)}
+                />
+              </label>
+            </div>
+            <div className='new-drop-field'>
+              <label>
+                <input
+                  class='new-drop-overlay-input'
+                  type='text'
+                  id='last-name-input'
+                  name='lname'
+                  value={this.state.lname}
+                  required
+                  placeholder='Last Name'
+                  onChange={e => this.handleChange(e)}
+                />
+              </label>
+            </div>
+            <div className='new-drop-field'>
+              <label>
+                <input
+                  class='new-drop-overlay-input'
+                  type='text'
+                  id='email-input'
+                  name='email'
+                  value={this.state.email}
+                  required
+                  placeholder='Email'
+                  onChange={e => this.handleChange(e)}
+                />
+              </label>
+            </div>
+            {/* <div className='subscribe_form-submit-btn' onClick={this.onSubscriptionButtonClick}>
+              <h4>{this.state.buttonText}</h4>
+            </div>
+            {this.state.showFlashMsg && (
+              <div>
+                <FlashMessage duration={5000}>
+                  <h4>{this.state.flashMsgTxt}</h4>
+                </FlashMessage>
+              </div>
+            )} */}
+            </div>
+          <div className='new-drop-overlay-order-btn' onClick={this.onSubscriptionButtonClick}>
+            <h4>SUBSCRIBE NOW</h4>
           </div>
+          {this.state.showFlashMsg && (
+              <div>
+                <FlashMessage duration={5000}>
+                  <h4>{this.state.flashMsgTxt}</h4>
+                </FlashMessage>
+              </div>
+            )}
         </div>
       </div>
     );
