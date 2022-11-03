5996c540c46d28e92049266c6f78ae7a586c416e
Hyo Won Jeong <hwjeong@berkeley.edu>
2022-11-02 17:03:46 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 //Email Info
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
