f4f045ba8cdd9d59c980c0a7a9fc34adbd7ec41a
sechoi1300 <sechoi1300@gmail.com>
2023-04-01 00:48:33 -0700

    RESET

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,7 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import '../styles/Main.css';
 
-import Navbar from './Navbar';
-import MobileNav from './MobileNav';
-
 import HomePage from '../pages/HomePage';
-import AboutPage from '../pages/AboutPage';
-import StaffPage from '../pages/StaffPage';
-import DesignsPage from '../pages/DesignsPage';
-import ShopPage from '../pages/ShopPage';
-import PartnershipsPage from '../pages/PartnershipsPage';
-import ProductPage from '../pages/ProductPage';
-import CheckoutPage from '../pages/CheckoutPage';
 import { useMediaQuery } from 'react-responsive';
 
 const Main = ({ location }) => {
@@ -25,29 +15,22 @@ const Main = ({ location }) => {
 
   return (
     <Fragment>
-      {isDesktopOrLaptop && (
+      {/*isDesktopOrLaptop && (
         <>
           <Navbar />
         </>
-      )}
+      )*/}
 
-      {isMobile && (
+      {/*isMobile && (
         <>
           <MobileNav />
         </>
-      )}
+      )*/}
       <TransitionGroup className='transition-group'>
         <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={'fade'}>
           <section className='route-section'>
             <Switch location={location}>
               <Route exact path='/' component={HomePage} />
-              <Route path='/about' component={AboutPage} />
-              <Route path='/staff' component={StaffPage} />
-              <Route path='/designs' component={DesignsPage} />
-              <Route path='/shop' component={ShopPage} />
-              <Route path='/partnerships' component={PartnershipsPage} />
-              <Route path='/product' component={ProductPage} />
-              <Route path='/checkout' component={CheckoutPage} />
             </Switch>
           </section>
         </CSSTransition>
